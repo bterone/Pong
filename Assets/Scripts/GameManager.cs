25e8fc135784195e6d6771da8f8cd219528de349
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-04-07 14:09:21 +0530

    Deleted unecessary code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,6 @@ public class GameManager : MonoBehaviour
     public static int PlayerScore1 = 0;
     public static int PlayerScore2 = 0;
 
-    public GUISkin layout;
-
     public GameObject theBall;
     public static Text P1Score, P2Score, playerWin;
 
@@ -29,14 +27,14 @@ public class GameManager : MonoBehaviour
         if (PlayerScore1 == 10)
         {
             playerWin.text = "PLAYER ONE WIN";
-            GameObject.FindGameObjectWithTag("Ball").
-                SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+            /*GameObject.FindGameObjectWithTag("Ball").
+                SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);*/
         }
         else if (PlayerScore2 == 10)
         {
             playerWin.text = "PLAYER TWO WIN";
-            GameObject.FindGameObjectWithTag("Ball").
-                SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+            /*GameObject.FindGameObjectWithTag("Ball").
+                SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);*/
         }
         else
         {
@@ -50,31 +48,6 @@ public class GameManager : MonoBehaviour
         PlayerScore2 = 0;
         theBall.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
     }
-
-    /*private void OnGUI()
-    {
-        GUI.skin = layout;
-        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
-        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
-
-        if (GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 53), "RESTART"))
-        {
-            PlayerScore1 = 0;
-            PlayerScore2 = 0;
-            theBall.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
-        }
-
-        if (PlayerScore1 == 10)
-        {
-            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
-            theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
-        }
-        else if (PlayerScore2 == 10)
-        {
-            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
-            theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
-        }
-    }*/
 
     void Start()
     {
